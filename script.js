084962ef6cd575c0e3626232adfb0388098f02e9
Anton Khlebnikov <toni.farsh@icloud.com>
2023-03-06 10:48:12 +0500

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.typing-text', {
+	strings: ['начинающий Фронтенд Разработчик', 'чёткий парень'],
+  typeSpeed: 60,
+  backSpeed: 30,
+  loop: true,
+  showCursor: true,
+  });
