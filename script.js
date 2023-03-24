32de8fa6b459d85504ef807df9993ab0669a4b61
Anton Khlebnikov <toni.farsh@icloud.com>
2023-03-24 16:27:27 +0500

    reload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.typing-text', {
-	strings: ['Фронтенд Разработчик', 'Чёткий парень'],
+	strings: ['Frontend Разработчик', 'добрый добряк', 'твой друг'],
   typeSpeed: 60,
   backSpeed: 30,
   loop: true,
